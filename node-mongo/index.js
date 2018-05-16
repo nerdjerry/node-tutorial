9a27255fbd5a76c4ae34152fb642a9148e2c5d88
Prateek Singhal <lucifer@Prateeks-MacBook-Pro.local>
2018-05-16 22:08:21 +0530

    Repair callback hell with promises

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -4,29 +4,27 @@ const dboper = require('./operations');
 
 const url = 'mongodb://localhost:27017/conFusion';
 
-MongoClient.connect(url, (err, client) => {
-    assert.equal(err,null);
-
+MongoClient.connect(url)
+.then((client) => {
     console.log("Connected succesfully to server");
     const db = client.db();
     const collection = 'dishes';
-    dboper.insertDocument(db, {'name':'Rajma', 'description': 'Mother of dishes'}, collection, (result) => {
-        dboper.findDocument(db,collection, (result) => {
-            console.log(result);
-            dboper.updateDocument(db, {'name':'Rajma'} , {'description': 'Best dish ever'}, collection, (result) => {
-                console.log('result updated with', result.result);
-                dboper.findDocument(db,collection, (result) => {
-                    console.log(result);
-                    dboper.removeDocument(db, {'name':'Rajma'}, collection, (result) => {
-                        console.log('Successfully removed');
-                        db.dropCollection(collection, (err, result) => {
-                            assert.equal(err,null);
-                            client.close();
-                        })
-                    })
-                })
-                
-            })
-        })
-    })
+    dboper.insertDocument(db, {'name':'Rajma', 'description': 'Mother of dishes'}, collection)
+    .then((result) => {
+        return dboper.findDocument(db,collection)})
+    .then((result) => {
+        console.log(result);
+        return dboper.updateDocument(db, {'name':'Rajma'} , {'description': 'Best dish ever'}, collection)})
+    .then((result) => {
+        console.log('result updated with', result.result);
+        return dboper.findDocument(db,collection)})
+    .then((result) => {
+        console.log(result);
+        return dboper.removeDocument(db, {'name':'Rajma'}, collection)})
+    .then((result) => {
+        console.log('Successfully removed');
+        return db.dropCollection(collection)})
+    .then((result) => {
+        client.close();
+    });
 });
