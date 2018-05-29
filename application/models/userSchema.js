5f600d412beb7f8ec55a69efbe9c71d38312e76b
lucifer <psinghal4210@gmail.com>
2018-05-29 23:38:58 +0530

    Add firstname and lastname to userSchema

--- application/models/userSchema.js
+++ application/models/userSchema.js
@@ -3,11 +3,19 @@ var Schema = mongoose.Schema;
 var passportMongoose = require('passport-local-mongoose');
 
 var User = new Schema({
-    admin : {
-        type : Boolean,
-        default : false
+    firstname : {
+        type : String,
+        required : true
+    },
+    lastname: {
+        type : String, 
+        required : true
+    },
+    admin: {
+        type: Boolean,
+        default: false
     }
 });
 
 User.plugin(passportMongoose);
-module.exports = mongoose.model('User',User);
+module.exports = mongoose.model('User', User);
