c9ab6f6df42206bb6191de7f8ec12d51f8da0d6d
lucifer <psinghal4210@gmail.com>
2018-06-03 01:48:20 +0530

    Setup filestorage and filter options for multer

--- application/routes/uploadRouter.js
+++ application/routes/uploadRouter.js
@@ -3,3 +3,23 @@ const uploadRouter = express.Router();
 const bodyParser = require('body-parser');
 const multer = require('multer');
 const authenticate = require('../authenticate');
+
+const options = {}
+
+options.storage = multer.diskStorage({
+    destination : (req,file,cb) => {
+        cb(null,'public/images');
+    },
+    filename: (req,file,cb) =>{
+        cb(null, file.originalname);
+    }
+});
+
+options.fileFilter = (req,file,cb) => {
+    if(!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)){
+        cb(new Error("File format not supported"));
+    }
+    cb(null, true);
+}
+
+var upload = multer(options)
