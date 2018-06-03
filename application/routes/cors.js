8e35ffaf898ea1d4380e61bd517e0d2319f3c1a3
lucifer <psinghal4210@gmail.com>
2018-06-04 01:05:52 +0530

    Set status code to send with options

--- application/routes/cors.js
+++ application/routes/cors.js
@@ -4,14 +4,14 @@ const cors = require('cors');
 const whitelist = ["http://localhost:3000","https://locahost:3443"];
 var corsOptionsDelegate = (req, callback) =>{
     var corsOptions;
-    if(whitelist.indexOf(req.headers('Origin')) != -1){
+    if(whitelist.indexOf(req.header('Origin')) !== -1){
         //This means domain requested from is a whitelisted domanin
-        corsOptions = {origin : true};
+        corsOptions = {origin : true, optionsSuccessStatus : 200};
     }else {
         corsOptions = {origin : false};
     }
     callback(null, corsOptions);
-}
+};
 
-module.exports.cors = cors()
+module.exports.cors = cors();
 module.exports.corsWithOptions = cors(corsOptionsDelegate);
