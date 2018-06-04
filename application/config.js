df026d36f7e34952b97228f791e98f7c33ed7065
lucifer <psinghal4210@gmail.com>
2018-06-05 01:38:10 +0530

    Add client id and client secret to configuration list

--- application/config.js
+++ application/config.js
@@ -1,2 +1,4 @@
 exports.secret = "12344-2131-1231231";
-exports.mongourl = "mongodb://localhost:27017/conFusion"
+exports.mongourl = "mongodb://localhost:27017/conFusion";
+exports.clientId = "232846434143675";
+exports.clientSecret = "361f12399971227c5d033ff18c71817a";
