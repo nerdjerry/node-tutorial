d02a5dc5d1dc7cedd599ff312a727edd690808d5
lucifer <psinghal4210@gmail.com>
2018-06-25 23:29:16 +0530

    Setting up graphQL Server

---
+++ graphql-app/src/index.js
@@ -0,0 +1,16 @@
+const {GraphQLServer} = require('graphql-yoga');
+
+const typeDefs = `
+type Query{
+    info: String!
+}
+`;
+
+const resolvers = {
+    Query : {
+        info : () => "This is test API",
+    },
+};
+
+const server = new GraphQLServer({typeDefs, resolvers});
+server.start(() => console.log("Server is running"));
