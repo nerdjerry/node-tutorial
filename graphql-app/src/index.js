528a47647d3c80f3a77d4131eedc4037d1f1c6c0
lucifer <psinghal4210@gmail.com>
2018-06-26 20:54:18 +0530

    Add link type and resolvers

--- graphql-app/src/index.js
+++ graphql-app/src/index.js
@@ -2,14 +2,31 @@ const {GraphQLServer} = require('graphql-yoga');
 
 const typeDefs = `
 type Query{
-    info: String!
+    info: String!,
+    feed : [Link!]!,
+}
+type Link {
+    id: ID!,
+    description: String!,
+    url: String!
 }
 `;
-
+const list = [{
+    id : 3223,
+    description : "This is a link",
+    url : "google.com"
+}]
 const resolvers = {
     Query : {
         info : () => "This is test API",
+        feed : () => list,
     },
+    //Optional
+    Link : {
+        id : (root) => root.id,
+        description : (root) => root.description,
+        url: (root) => root.url,
+    }
 };
 
 const server = new GraphQLServer({typeDefs, resolvers});
