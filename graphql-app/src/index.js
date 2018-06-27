7289c3fc4400fe22d8f0cf28628e610b0a69b917
lucifer <psinghal4210@gmail.com>
2018-06-27 22:28:23 +0530

    Add CRUD functionality to a particular link identified by id

--- graphql-app/src/index.js
+++ graphql-app/src/index.js
@@ -1,6 +1,6 @@
 const { GraphQLServer } = require("graphql-yoga");
 
-const list = [
+let list = [
   {
     id: 0,
     description: "This is a link",
@@ -11,7 +11,14 @@ let counter = list.length;
 const resolvers = {
   Query: {
     info: () => "This is test API",
-    feed: () => list
+    feed: () => list,
+    link: (root,args) => {
+        for( let link of list) {
+            if(link.id == args.id){
+                return link
+            }
+        }
+    }
   },
   //Optional
   Link: {
@@ -28,6 +35,26 @@ const resolvers = {
         };
         list.push(link);
         return link;
+    },
+    updateLink : (root,args) => {
+        for(let link of list){
+            if(link.id == args.id){
+                link.description = args.description,
+                link.url = args.url
+                return link
+            }
+        }
+    },
+    deleteLink : (root, args) => {
+        let deletedLink
+        list = list.filter(((value) => {
+            if(value.id != args.id){
+                return value
+            }else{
+                deletedLink = value;
+            }
+        }));
+        return deletedLink;
     }
   }
 };
