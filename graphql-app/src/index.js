bf1d2b69922adfef0115fcae7521f1fc45cd99f5
lucifer <psinghal4210@gmail.com>
2018-06-29 22:44:25 +0530

    Use Prisma to talk to Prisma database

--- graphql-app/src/index.js
+++ graphql-app/src/index.js
@@ -1,66 +1,69 @@
 const { GraphQLServer } = require("graphql-yoga");
+const Prisma = require('prisma-binding').Prisma;
 
-let list = [
-  {
-    id: 0,
-    description: "This is a link",
-    url: "google.com"
-  }
-];
-let counter = list.length;
 const resolvers = {
   Query: {
     info: () => "This is test API",
-    feed: () => list,
-    link: (root,args) => {
-        for( let link of list) {
-            if(link.id == args.id){
-                return link
-            }
-        }
+    feed: (root,args,context,info) => {
+        return context.db.query.links({},info);
     }
+    // link: (root,args) => {
+    //     for( let link of list) {
+    //         if(link.id == args.id){
+    //             return link
+    //         }
+    //     }
+    // }
   },
   //Optional
-  Link: {
-    id: root => root.id,
-    description: root => root.description,
-    url: root => root.url
-  },
+//   Link: {
+//     id: root => root.id,
+//     description: root => root.description,
+//     url: root => root.url
+//   },
   Mutation: {
-    post: (root,args) => {
-        var link = {
-            id : counter++,
-            description : args.description,
-            url: args.url
-        };
-        list.push(link);
-        return link;
-    },
-    updateLink : (root,args) => {
-        for(let link of list){
-            if(link.id == args.id){
-                link.description = args.description,
-                link.url = args.url
-                return link
+    post: (root,args,context,info) => {
+        return context.db.mutation.createLink({
+            data: {
+                description: args.description,
+                url : args.url
             }
-        }
+        },info);
     },
-    deleteLink : (root, args) => {
-        let deletedLink
-        list = list.filter(((value) => {
-            if(value.id != args.id){
-                return value
-            }else{
-                deletedLink = value;
-            }
-        }));
-        return deletedLink;
+//     updateLink : (root,args) => {
+//         for(let link of list){
+//             if(link.id == args.id){
+//                 link.description = args.description,
+//                 link.url = args.url
+//                 return link
+//             }
+//         }
+//     },
+//     deleteLink : (root, args) => {
+//         let deletedLink
+//         list = list.filter(((value) => {
+//             if(value.id != args.id){
+//                 return value
+//             }else{
+//                 deletedLink = value;
+//             }
+//         }));
+//         return deletedLink;
+//     } 
     }
-  }
 };
 
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
-  resolvers
+  resolvers,
+  context: req => ({
+      ...req,
+      db : new Prisma({
+          typeDefs : 'src/generated/prisma.graphql',
+          endpoint: 'https://us1.prisma.sh/public-yellowcentaur-956/hackernews-node/dev',
+          secret: 'mysecret123',
+          debug: true
+      })
+  })
 });
 server.start(() => console.log("Server is running"));
